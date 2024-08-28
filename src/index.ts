bfdb528d8acd72560a4ad2ecb9889761a28e80ef
Paolo Dapul <paolodapultech@gmail.com>
2024-08-28 12:51:06 +0800

    build: configure env var file path

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
-dotenv.config();
+const ENV = process.env.NODE_ENV ?? "development";
+
+dotenv.config({ path: `.env.${ENV}` });
 
 const app: Express = express();
 const port = process.env.PORT ?? 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to Nodecommerce!");
+  res.send(`Welcome to Nodecommerce - ${ENV}`);
 });
 
 app.listen(port, () => {
