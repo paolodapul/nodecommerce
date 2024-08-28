42580a6d6b9bee307481f950b3d6dc32fe57a061
Paolo Dapul <paolodapultech@gmail.com>
2024-08-28 13:23:54 +0800

    feat: apply cors whitelisting on prod

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors, { CorsOptions } from "cors";
 
 const ENV = process.env.NODE_ENV ?? "development";
 
@@ -8,6 +9,24 @@ dotenv.config({ path: `.env.${ENV}` });
 const app: Express = express();
 const port = process.env.PORT ?? 3000;
 
+const whitelist = ["https://prod.example.com"];
+const corsCondition = (origin: string | undefined) => {
+  return ENV !== "production"
+    ? !origin || whitelist.includes(origin)
+    : origin && whitelist.includes(origin);
+};
+
+const corsOptions: CorsOptions = {
+  origin: function (origin, callback) {
+    if (corsCondition(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 app.get("/", (req: Request, res: Response) => {
   res.send(`Welcome to Nodecommerce - ${ENV}`);
 });
