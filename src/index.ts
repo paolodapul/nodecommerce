be0da45b972dc1c789de99b1b7f96d3510473a0a
Paolo Dapul <paolodapultech@gmail.com>
2024-08-28 10:32:43 +0800

    fix: lint issues

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ?? 3000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Nodecommerce!");
 });
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(
+    `[server]: Server is running at http://localhost:${port as string}`
+  );
 });
