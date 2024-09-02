9ac6aec852cce0c5d71200bebaf56ca1e0fa432d
Paolo Dapul <paolodapultech@gmail.com>
2024-09-02 18:56:01 +0800

    feat: apply password hashing via bcrypt

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,4 +1,16 @@
 import { User } from "../models/userModel";
+import bcrypt from "bcryptjs";
+async function hashPassword(password: string): Promise<string> {
+  const saltRounds = 10;
+  try {
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hash = await bcrypt.hash(password, salt);
+    return hash;
+  } catch (error) {
+    console.error("Error hashing password:", error);
+    throw error;
+  }
+}
 
 type UserData = {
   username: string;
@@ -8,7 +20,11 @@ type UserData = {
 
 const register = async (userData: UserData) => {
   const { username, email, password } = userData;
-  const newUser = new User({ username, email, password });
+  const newUser = new User({
+    username,
+    email,
+    password: await hashPassword(password),
+  });
   await newUser.save();
   return newUser;
 };
