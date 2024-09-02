1d507f59a07d2df04908996e3c39dc234391f66c
Paolo Dapul <paolodapultech@gmail.com>
2024-09-02 10:38:36 +0800

    test: create integration test for cors

--- src/server/index.test.ts
+++ src/server/index.test.ts
@@ -1,5 +1,8 @@
 import request from "supertest";
 import { createApp } from ".";
+import { Express } from "express";
+
+const app: Express = createApp("DEV");
 
 describe("App", () => {
   const ENV = "development";
@@ -8,6 +11,20 @@ describe("App", () => {
   it("should respond to GET /", async () => {
     const response = await request(app).get("/");
     expect(response.status).toBe(200);
-    expect(response.text).toBe("Welcome to Nodecommerce");
+    expect(response.text).toBe(`Welcome to Nodecommerce - ${ENV}`);
+  });
+});
+
+/**
+ * Todo: make the CORS integration test more robust - learn how to test according to set environment
+ */
+
+describe("CORS Middleware", () => {
+  it("should not allow requests from an unspecified origin", async () => {
+    const response = await request(app)
+      .get("/")
+      .set("Origin", "http://example.com");
+
+    expect(response.headers["access-control-allow-origin"]).toBeUndefined();
   });
 });
