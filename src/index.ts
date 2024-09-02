48e300701323cd82d53973081d62b4289560833c
Paolo Dapul <paolodapultech@gmail.com>
2024-09-02 14:18:41 +0800

    feat: add graceful shutdown with database disconnection

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { IncomingMessage, Server, ServerResponse } from "http";
 import { createApp } from "./server";
 import dotenv from "dotenv";
 
@@ -7,8 +8,30 @@ dotenv.config({ path: `.env.${ENV}` });
 const port = process.env.PORT ?? 3000;
 const app = createApp(ENV);
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(
     `[server]: Server is running at http://localhost:${port as string}`
   );
+});
+
+async function gracefulShutdown(
+  signal: string,
+  server: Server<typeof IncomingMessage, typeof ServerResponse>,
+  disconnectDB: () => Promise<void>
+) {
+  console.log(`Received ${signal}. Closing HTTP server.`);
+  server.close(() => {
+    console.log("HTTP server closed");
+  });
+
+  await disconnectDB();
+  console.log("Process terminated");
+  process.exit(0);
+}
+
+process.on("SIGINT", () => {
+  void gracefulShutdown("SIGINT", server, async () => Promise.resolve());
+});
+process.on("SIGTERM", () => {
+  void gracefulShutdown("SIGTERM", server, async () => Promise.resolve());
 });
