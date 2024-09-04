f90a1b110168dc02a4161627a184a2d3608d9dc6
Paolo Dapul <paolodapultech@gmail.com>
2024-09-04 11:59:08 +0800

    feat: create middleware for checking permissions

---
+++ src/middleware/checkPermission.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+import { Role } from "../models/roleModel";
+
+interface CustomRequest extends Request {
+  user?: {
+    role: string;
+  };
+}
+
+function checkPermission(requiredPermission: string) {
+  return async (req: CustomRequest, res: Response, next: NextFunction) => {
+    const userRole = req.user?.role;
+    const role = await Role.findById(userRole);
+
+    if (!role) {
+      return res.status(403).json({ message: "Role not found." });
+    }
+
+    if (role.permissions.includes(requiredPermission)) {
+      next();
+    } else {
+      res.status(403).json({ message: "Action is forbidden." });
+    }
+  };
+}
+
+export { checkPermission };
