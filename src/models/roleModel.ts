ffa1066819c1bbf820079fc5539b9afc99ea5339
Paolo Dapul <paolodapultech@gmail.com>
2024-09-04 11:13:51 +0800

    feat: enhance role model schema

--- src/models/roleModel.ts
+++ src/models/roleModel.ts
@@ -1,8 +1,26 @@
 import mongoose from "mongoose";
 
 const roleSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  permissions: [{ type: String }],
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  permissions: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  description: {
+    type: String,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const Role = mongoose.model("Role", roleSchema);
