1ea0193383d24444bf44d2cd44c81bc72e5790ae
Paolo Dapul <paolodapultech@gmail.com>
2024-09-04 12:01:27 +0800

    feat: create private Product endpoints

---
+++ src/routes/private/productRoutes.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+/* eslint-disable @typescript-eslint/unbound-method */
+import express from "express";
+import productController from "../../controllers/productController";
+import { checkPermission } from "../../middleware/checkPermission";
+
+const router = express.Router();
+
+router.post(
+  "/",
+  checkPermission("create_products"),
+  productController.createProduct
+);
+router.put(
+  "/:id",
+  checkPermission("update_products"),
+  productController.updateProduct
+);
+router.delete(
+  "/:id",
+  checkPermission("delete_products"),
+  productController.deleteProduct
+);
+
+export default router;
