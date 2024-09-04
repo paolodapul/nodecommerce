58cddc9347da0dc89730e4a0d28b110011f23f9e
Paolo Dapul <paolodapultech@gmail.com>
2024-09-04 15:30:35 +0800

    feat: implement Order routes

--- src/routes/private/index.ts
+++ src/routes/private/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import productRoutes from "./productRoutes";
 import cartRoutes from "./cartRoutes";
+import orderRoutes from "./orderRoutes";
 
 const router = express.Router();
 router.use("/products", productRoutes);
 router.use("/cart", cartRoutes);
+router.use("/orders", orderRoutes);
 
 export default router;
---
+++ src/routes/private/orderRoutes.ts
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/unbound-method */
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import express from "express";
+import orderController from "../../controllers/orderController";
+
+const router = express.Router();
+
+/**
+ * Create order
+ */
+
+router.post("/", orderController.createOrder);
+
+/**
+ * Get order by ID
+ */
+
+router.get("/:orderId", orderController.getOrder);
+
+/**
+ * Get user orders
+ */
+
+router.get("/user/:userId", orderController.getUserOrders);
+
+/**
+ * Update order
+ */
+
+router.patch("/:orderId", orderController.updateOrder);
+
+/**
+ * Update order status
+ */
+
+router.patch("/:orderId/status", orderController.updateOrderStatus);
+
+/**
+ * Cancel order
+ */
+
+router.post("/:orderId/cancel", orderController.cancelOrder);
+
+export default router;
