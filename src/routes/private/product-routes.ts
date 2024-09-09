5aa7eae3c0c4c4d11cadec77b5585e7863863c20
Paolo Dapul <paolodapultech@gmail.com>
2024-09-09 17:02:30 +0800

    fix: remove permission checking in Product private routes

--- src/routes/private/product-routes.ts
+++ src/routes/private/product-routes.ts
@@ -2,24 +2,11 @@
 /* eslint-disable @typescript-eslint/unbound-method */
 import express from "express";
 import productController from "../../controllers/product-controller";
-import checkPermission from "../../middleware/check-permission";
 
 const router = express.Router();
 
-router.post(
-  "/",
-  checkPermission("create_products"),
-  productController.createProduct
-);
-router.put(
-  "/:id",
-  checkPermission("update_products"),
-  productController.updateProduct
-);
-router.delete(
-  "/:id",
-  checkPermission("delete_products"),
-  productController.deleteProduct
-);
+router.post("/", productController.createProduct);
+router.put("/:id", productController.updateProduct);
+router.delete("/:id", productController.deleteProduct);
 
 export default router;
