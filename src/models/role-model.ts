1eaf3343339d3adf05fcbe5cea98127b75d70f41
Paolo Dapul <paolodapultech@gmail.com>
2024-09-09 17:01:51 +0800

    feat: declare permissions on Role model

--- src/models/role-model.ts
+++ src/models/role-model.ts
@@ -1,5 +1,26 @@
 import mongoose from "mongoose";
 
+export const VALID_PERMISSIONS = [
+  "create_products",
+  "view_products",
+  "update_products",
+  "delete_products",
+  "view_orders",
+  "update_orders",
+  "cancel_orders",
+  "create_customer_order",
+  "view_customer_orders",
+  "cancel_customer_orders",
+] as const;
+
+export type Permission = (typeof VALID_PERMISSIONS)[number];
+
+interface IRole extends Document {
+  name: string;
+  permissions: Permission[];
+  description?: string;
+}
+
 const roleSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -9,8 +30,20 @@ const roleSchema = new mongoose.Schema({
   },
   permissions: [
     {
-      type: String,
+      type: [String],
+      enum: VALID_PERMISSIONS,
       required: true,
+      validate: [
+        {
+          validator: (array: Permission[]) => array.length > 0,
+          message: "At least one permission is required.",
+        },
+        {
+          validator: (array: Permission[]) =>
+            array.every((v) => VALID_PERMISSIONS.includes(v)),
+          message: "Invalid permission value.",
+        },
+      ],
     },
   ],
   description: {
@@ -23,6 +56,6 @@ const roleSchema = new mongoose.Schema({
   },
 });
 
-const Role = mongoose.model("Role", roleSchema);
+const Role = mongoose.model<IRole>("Role", roleSchema);
 
 export { Role };
