d8294d1caea0fd07d5e0425b2688013150e4c2e9
Paolo Dapul <paolodapultech@gmail.com>
2024-09-10 21:29:32 +0800

    fix: log origin

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -4,6 +4,8 @@ const ENV = process.env.NODE_ENV ?? "development";
 
 const whitelist = [process.env.EC2_PUBLIC_DNS];
 const corsCondition = (origin: string | undefined) => {
+  console.log("[origin]", origin);
+
   return ENV !== "production"
     ? !origin || whitelist.includes(origin)
     : origin && whitelist.includes(origin);
