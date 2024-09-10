d11d520d10f5a76fb6afb3458ef4eed8e114b860
Paolo Dapul <paolodapultech@gmail.com>
2024-09-10 22:23:37 +0800

    fix: allow non-browser clients to bypass CORS

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -1,19 +1,10 @@
 import cors, { CorsOptions } from "cors";
 
-const ENV = process.env.NODE_ENV ?? "development";
-
 const whitelist = [process.env.EC2_PUBLIC_DNS];
-const corsCondition = (origin: string | undefined) => {
-  console.log("[origin]", origin);
-
-  return ENV !== "production"
-    ? !origin || whitelist.includes(origin)
-    : origin && whitelist.includes(origin);
-};
 
 const corsOptions: CorsOptions = {
   origin: function (origin, callback) {
-    if (corsCondition(origin)) {
+    if (!origin || whitelist.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
