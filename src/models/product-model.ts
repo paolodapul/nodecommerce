e249bfd5300deb1ae9c4080c06375e72994da2a9
Paolo Dapul <paolodapultech@gmail.com>
2024-09-11 19:05:21 +0800

    feat: model data for Product category and reviews

--- src/models/product-model.ts
+++ src/models/product-model.ts
@@ -1,9 +1,20 @@
 import mongoose from "mongoose";
 
+const reviewSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    message: { type: String },
+    rating: { type: Number, required: true, min: 1, max: 5 },
+  },
+  { timestamps: true }
+);
+
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
   description: { type: String },
+  category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+  reviews: [reviewSchema],
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
