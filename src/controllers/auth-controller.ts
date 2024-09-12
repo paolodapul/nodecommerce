c34b113c27f42ed226bd686e11d53ba992b7fbd6
Paolo Dapul <paolodapultech@gmail.com>
2024-09-12 14:10:43 +0800

    refactor: modularize user registration logic

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -1,9 +1,13 @@
 import { Request, Response } from "express";
 import { z } from "zod";
 import { User } from "../models/user-model";
-import { verifyPassword, hashPassword } from "../utils/hashing";
+import { verifyPassword } from "../utils/hashing";
 import jwt from "jsonwebtoken";
-import { Role } from "../models/role-model";
+import {
+  createCustomer,
+  RegisterBody,
+  validateRegistrationRequest,
+} from "../core/auth";
 
 const loginSchema = z
   .object({
@@ -14,64 +18,11 @@ const loginSchema = z
 
 type LoginBody = z.infer<typeof loginSchema>;
 
-const registerSchema = z
-  .object({
-    username: z
-      .string()
-      .min(3, "Username must be at least 3 characters long")
-      .max(30, "Username must not exceed 30 characters")
-      .regex(
-        /^[a-zA-Z0-9_-]+$/,
-        "Username can only contain letters, numbers, underscores, and hyphens"
-      )
-      .refine(
-        (username) => !username.startsWith("-") && !username.endsWith("-"),
-        "Username cannot start or end with a hyphen"
-      ),
-    email: z.string().email(),
-    password: z.string().min(8),
-  })
-  .strict();
-
-type RegisterBody = z.infer<typeof registerSchema>;
-
 class AuthController {
   async register(req: Request, res: Response) {
     try {
-      const result = registerSchema.safeParse(req.body);
-
-      if (!result.success) {
-        const errorMessages = result.error.flatten().fieldErrors;
-        return res.status(400).json({ errors: errorMessages });
-      }
-
-      const { username, email, password } = req.body as RegisterBody;
-      const hashedPassword = await hashPassword(password);
-
-      const customerPermissions = [
-        "create_order",
-        "view_orders",
-        "update_orders",
-        "cancel_orders",
-        "add_to_cart",
-        "view_cart",
-        "update_cart",
-        "remove_from_cart",
-        "create_review",
-      ];
-
-      const customerRoles = await Role.find({
-        permissions: { $in: customerPermissions },
-      });
-
-      const newUser = new User({
-        username,
-        email,
-        password: hashedPassword,
-        roles: customerRoles,
-      });
-      await newUser.save();
-
+      validateRegistrationRequest(req);
+      await createCustomer(req.body as RegisterBody);
       res.status(200).json({ description: "Registration successful!" });
     } catch (error) {
       res.status(500).json({
