28d7572d5ec613cfbe77a4a320a4c2dee8806c3c
Paolo Dapul <paolodapultech@gmail.com>
2024-09-16 15:02:53 +0800

    refactor: implement login input validation using validator util function

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -1,13 +1,9 @@
 import { Request, Response } from "express";
-import {
-  createCustomer,
-  generateToken,
-  validateLoginRequest,
-  verifyUser,
-} from "../core/auth";
+import { createCustomer, generateToken, verifyUser } from "../core/auth";
 import { validate, ValidationError } from "../utils/validator";
 import {
   LoginInput,
+  loginSchema,
   RegisterInput,
   registerSchema,
 } from "../schemas/auth-schema";
@@ -35,16 +31,23 @@ class AuthController {
 
   async login(req: Request, res: Response) {
     try {
-      validateLoginRequest(req);
+      validate(loginSchema, req.body);
       const userData = req.body as LoginInput;
       const validUser = await verifyUser(userData);
       const token = generateToken(validUser);
       res.status(200).json({ description: "Log in successful.", token });
     } catch (error) {
-      res.status(500).json({
-        error: "Login failed",
-        message: (error as Error).message,
-      });
+      if (error instanceof ValidationError) {
+        res.status(500).json({
+          error: "Login failed",
+          message: error.errorMessages,
+        });
+      } else {
+        res.status(500).json({
+          error: "Login failed",
+          message: (error as Error).message,
+        });
+      }
     }
   }
 }
