1bc4d9ec9763728b0ab050c171528e428bae2a14
Paolo Dapul <paolodapultech@gmail.com>
2024-09-16 16:25:42 +0800

    feat: apply error handler and HTTP types in Auth controller

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -1,53 +1,40 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { createCustomer, generateToken, verifyUser } from "../core/auth";
-import { validate, ValidationError } from "../utils/validator";
+import { validate } from "../utils/validator";
 import {
   LoginInput,
   loginSchema,
   RegisterInput,
   registerSchema,
 } from "../schemas/auth-schema";
+import { BadRequestException } from "../types/error-types";
 
 class AuthController {
-  async register(req: Request, res: Response) {
+  async register(req: Request, res: Response, next: NextFunction) {
     try {
-      validate(registerSchema, req.body);
+      const validationResult = validate(registerSchema, req.body);
+      if (!validationResult.success) {
+        throw new BadRequestException(validationResult.errors);
+      }
       await createCustomer(req.body as RegisterInput);
       res.status(200).json({ description: "Registration successful!" });
     } catch (error) {
-      if (error instanceof ValidationError) {
-        res.status(500).json({
-          error: "Registration failed",
-          message: error.errorMessages,
-        });
-      } else {
-        res.status(500).json({
-          error: "Registration failed",
-          message: (error as Error).message,
-        });
-      }
+      next(error);
     }
   }
 
-  async login(req: Request, res: Response) {
+  async login(req: Request, res: Response, next: NextFunction) {
     try {
-      validate(loginSchema, req.body);
+      const validationResult = validate(loginSchema, req.body);
+      if (!validationResult.success) {
+        throw new BadRequestException(validationResult.errors);
+      }
       const userData = req.body as LoginInput;
       const validUser = await verifyUser(userData);
       const token = generateToken(validUser);
       res.status(200).json({ description: "Log in successful.", token });
     } catch (error) {
-      if (error instanceof ValidationError) {
-        res.status(500).json({
-          error: "Login failed",
-          message: error.errorMessages,
-        });
-      } else {
-        res.status(500).json({
-          error: "Login failed",
-          message: (error as Error).message,
-        });
-      }
+      next(error);
     }
   }
 }
