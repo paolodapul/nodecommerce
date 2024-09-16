ddcf0e985f62f042b4a0f2912e59d64287185d61
Paolo Dapul <paolodapultech@gmail.com>
2024-09-16 13:46:02 +0800

    refactor: type annotations for Product model

--- src/models/product-model.ts
+++ src/models/product-model.ts
@@ -1,29 +1,30 @@
-import mongoose from "mongoose";
+import { Schema, model } from "mongoose";
+import { ICategory, IProduct } from "../types/ProductTypes";
 
-const categorySchema = new mongoose.Schema({
+const categorySchema = new Schema({
   name: { type: String, required: true },
 });
 
-const reviewSchema = new mongoose.Schema(
+const reviewSchema = new Schema(
   {
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
     message: { type: String },
     rating: { type: Number, required: true, min: 1, max: 5 },
   },
   { timestamps: true }
 );
 
-const productSchema = new mongoose.Schema({
+const productSchema = new Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
   description: { type: String },
-  category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+  category: { type: Schema.Types.ObjectId, ref: "Category" },
   reviews: [reviewSchema],
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
 
-const Category = mongoose.model("Category", categorySchema);
-const Product = mongoose.model("Product", productSchema);
+const Category = model<ICategory>("Category", categorySchema);
+const Product = model<IProduct>("Product", productSchema);
 
 export { Category, Product };
---
+++ src/types/ProductTypes.ts
@@ -0,0 +1,21 @@
+import { Document, Types } from "mongoose";
+
+export interface ICategory extends Document {
+  name: string;
+}
+
+export interface IReview {
+  user: Types.ObjectId;
+  message: string;
+  rating: number;
+}
+
+export interface IProduct extends Document {
+  name: string;
+  price: number;
+  description: string;
+  category: Types.ObjectId;
+  reviews: IReview[];
+  createdAt: Date;
+  updatedAt: Date;
+}
