931f8dbda951ab3266ef3712bf202d3aaaa1b834
Paolo Dapul <paolodapultech@gmail.com>
2024-09-16 14:54:38 +0800

    feat: use the Zod validator on register function

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -2,31 +2,41 @@ import { Request, Response } from "express";
 import {
   createCustomer,
   generateToken,
-  LoginBody,
-  RegisterBody,
   validateLoginRequest,
-  validateRegistrationRequest,
   verifyUser,
 } from "../core/auth";
+import { validate, ValidationError } from "../utils/validator";
+import {
+  LoginInput,
+  RegisterInput,
+  registerSchema,
+} from "../schemas/auth-schema";
 
 class AuthController {
   async register(req: Request, res: Response) {
     try {
-      validateRegistrationRequest(req);
-      await createCustomer(req.body as RegisterBody);
+      validate(registerSchema, req.body);
+      await createCustomer(req.body as RegisterInput);
       res.status(200).json({ description: "Registration successful!" });
     } catch (error) {
-      res.status(500).json({
-        error: "Registration failed",
-        message: (error as Error).message,
-      });
+      if (error instanceof ValidationError) {
+        res.status(500).json({
+          error: "Registration failed",
+          message: error.errorMessages,
+        });
+      } else {
+        res.status(500).json({
+          error: "Registration failed",
+          message: (error as Error).message,
+        });
+      }
     }
   }
 
   async login(req: Request, res: Response) {
     try {
       validateLoginRequest(req);
-      const userData = req.body as LoginBody;
+      const userData = req.body as LoginInput;
       const validUser = await verifyUser(userData);
       const token = generateToken(validUser);
       res.status(200).json({ description: "Log in successful.", token });
