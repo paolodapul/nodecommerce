e00905b5646d576fa3997c987b7f25e3d6b5dc43
Paolo Dapul <paolodapultech@gmail.com>
2024-09-16 10:15:28 +0800

    chore: cleanup

--- src/controllers/cart-controller.ts
+++ src/controllers/cart-controller.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Request, Response } from "express";
 import { Product } from "../models/product-model";
 import Cart, { ICart, ICartItem } from "../models/cart-model";
