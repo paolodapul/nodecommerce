8d021980c8ca8bdd1fbcbb1813dfc889057aed7c
Paolo Dapul <paolodapultech@gmail.com>
2024-09-17 09:43:03 +0800

    feat: create asyncHandler utility function

---
+++ src/utils/async-handler.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from "express";
+
+const asyncHandler = (
+  fn: (req: Request, res: Response, next: NextFunction) => Promise<unknown>
+) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+};
+
+export { asyncHandler };
