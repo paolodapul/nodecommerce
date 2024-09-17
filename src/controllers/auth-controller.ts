7bd40123462ceaa41f59dae1d8a81fe38f045620
Paolo Dapul <paolodapultech@gmail.com>
2024-09-17 09:43:39 +0800

    refactor: apply functional style to Auth controller

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -9,34 +9,38 @@ import {
 } from "../schemas/auth-schema";
 import { BadRequestException } from "../types/error-types";
 
-class AuthController {
-  async register(req: Request, res: Response, next: NextFunction) {
-    try {
-      const validationResult = validate(registerSchema, req.body);
-      if (!validationResult.success) {
-        throw new BadRequestException(validationResult.errors);
-      }
-      await createCustomer(req.body as RegisterInput);
-      res.status(200).json({ description: "Registration successful!" });
-    } catch (error) {
-      next(error);
+export const register = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const validationResult = validate(registerSchema, req.body);
+    if (!validationResult.success) {
+      throw new BadRequestException(validationResult.errors);
     }
+    await createCustomer(req.body as RegisterInput);
+    res.status(200).json({ description: "Registration successful!" });
+  } catch (error) {
+    next(error);
   }
+};
 
-  async login(req: Request, res: Response, next: NextFunction) {
-    try {
-      const validationResult = validate(loginSchema, req.body);
-      if (!validationResult.success) {
-        throw new BadRequestException(validationResult.errors);
-      }
-      const userData = req.body as LoginInput;
-      const validUser = await verifyUser(userData);
-      const token = generateToken(validUser);
-      res.status(200).json({ description: "Log in successful.", token });
-    } catch (error) {
-      next(error);
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const validationResult = validate(loginSchema, req.body);
+    if (!validationResult.success) {
+      throw new BadRequestException(validationResult.errors);
     }
+    const userData = req.body as LoginInput;
+    const validUser = await verifyUser(userData);
+    const token = generateToken(validUser);
+    res.status(200).json({ description: "Log in successful.", token });
+  } catch (error) {
+    next(error);
   }
-}
-
-export default new AuthController();
+};
