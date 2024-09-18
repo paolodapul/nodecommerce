84448c43154278735a7d12e073c51ecaca9b5569
Paolo Dapul <paolodapultech@gmail.com>
2024-09-18 21:27:06 +0800

    feat: create CheckoutRequest type to add order ID to request body type

--- src/types/payment-types.ts
+++ src/types/payment-types.ts
@@ -1,5 +1,14 @@
+import { Request } from "express";
+import { AuthRequest } from "./auth-types";
+
 export type WebhookBody = string | Buffer;
 export type LineItem = {
   price: string;
   quantity: number;
 };
+
+export interface CheckoutRequest extends Request, AuthRequest {
+  body: {
+    orderId: string;
+  };
+}
