6e9ba09ff624ea094576fdef7a9499efa57b4726
Paolo Dapul <paolodapultech@gmail.com>
2024-09-18 22:30:35 +0800

    feat: add log message after Cart and Order collection truncation

--- src/seeder/truncate-carts.ts
+++ src/seeder/truncate-carts.ts
@@ -1,7 +1,9 @@
 import { Cart } from "../models/cart-model";
+import logger from "../utils/logger";
 
 async function truncateCarts() {
   await Cart.deleteMany({});
+  logger.info("Cart data has been truncated.");
 }
 
 export { truncateCarts };
--- src/seeder/truncate-orders.ts
+++ src/seeder/truncate-orders.ts
@@ -1,7 +1,9 @@
 import { Order } from "../models/order-model";
+import logger from "../utils/logger";
 
 async function truncateOrders() {
   await Order.deleteMany({});
+  logger.info("Order data has been truncated.");
 }
 
 export { truncateOrders };
