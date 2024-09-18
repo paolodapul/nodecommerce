e6e2c3b13a9ad3aefcb506377cc5060ed2af16ba
Paolo Dapul <paolodapultech@gmail.com>
2024-09-18 18:20:01 +0800

    feat: pass cart items as argument to Stripe payment session

--- src/controllers/payment-controller.ts
+++ src/controllers/payment-controller.ts
@@ -1,14 +1,23 @@
 import { Request, Response, NextFunction } from "express";
 import * as StripeService from "../services/stripe";
 import { WebhookBody } from "../types/payment-types";
+import * as CartCore from "../core/cart";
+import { AuthRequest } from "../types/auth-types";
+import { UnauthorizedException } from "../types/error-types";
 
 export const checkout = async (
-  req: Request,
+  req: AuthRequest,
   res: Response,
   next: NextFunction
 ) => {
   try {
-    const session = await StripeService.createPaymentSession();
+    if (!req.user) {
+      throw new UnauthorizedException("User not found.");
+    }
+
+    const cart = await CartCore.getCart(req.user.id);
+    const lineItems = StripeService.transformCartToLineItems(cart);
+    const session = await StripeService.createPaymentSession(lineItems);
     res.status(201).json(session);
   } catch (error) {
     next(error);
