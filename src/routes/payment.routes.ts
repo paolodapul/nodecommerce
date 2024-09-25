7c13f2b4437c8a7228e2e844c9ebce1306f216a9
Paolo Dapul <paolodapultech@gmail.com>
2024-09-25 19:59:30 +0800

    feat: create endpoint for Stripe payment success

--- src/routes/payment.routes.ts
+++ src/routes/payment.routes.ts
@@ -1,11 +1,13 @@
-import express from "express";
-import * as paymentController from "../controllers/payment.controller";
-import { authorize, protect } from "../middleware/auth";
+import express from 'express';
+import * as paymentController from '../controllers/payment.controller';
+import { authorize, protect } from '../middleware/auth';
 
 const router = express.Router();
 
 router
-  .route("/checkout")
-  .post(protect, authorize("buyer"), paymentController.checkout);
+  .route('/checkout')
+  .post(protect, authorize('buyer'), paymentController.checkout);
+
+router.route('/success').get(paymentController.handlePaymentSuccess);
 
 export default router;
