dad931dc8c22023f2157267b77b1744406b5a5b5
Paolo Dapul <paolodapultech@gmail.com>
2024-09-25 20:04:13 +0800

    feat: add env vars to config

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,6 +4,10 @@ dotenv.config();
 
 export const config = {
   nodeEnv: process.env.NODE_ENV || 'development',
+  host: process.env.HOST || 'localhost',
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY || 'stripe_secret_key',
+  stripeWebhookSecret:
+    process.env.STRIPE_WEBHOOK_SECRET || 'stripe_webhook_secret',
   port: process.env.PORT || 3000,
   mongodbUri: process.env.MONGODB_URI || 'mongodb://localhost:27017/myapp',
   jwtSecret: process.env.JWT_SECRET || 'your_jwt_secret',
